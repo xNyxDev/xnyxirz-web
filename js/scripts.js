4b78e151ca08982c50d6a4854c5c4d34b882edcd
xNyxDev <xnyxdev@gmail.com>
2024-09-27 18:42:50 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.getElementById("modal").style.display = "flex";
+    document.getElementById("accept").onclick = function() {
+        document.getElementById("modal").style.display = "none";
+    };
+};
